4b3f5f525be1cfa5f176bdcf8ba60a83e9ef1611
Sufiyan Munshi <101869552+sufiyan02@users.noreply.github.com>
2025-02-14 12:55:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 document.getElementById("input[type='button']").addEventListener('click',()=>{
 	let s = document.getElementById('colorSelect');
-	let so = select.option[select.selectedIndex];
+	let so = select.options[select.selectedIndex];
 
 	if(so){
 		so.remove();
